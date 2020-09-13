d449292772f8ce589f5c5ee10375e235788b2447
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-09-13 14:42:41 -0300

    Ground Movement

---
+++ New Unity Project/Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //flap
+            rb.velocity = Vector2.up * speed;  // (0,1)
+        }
+    }
+}
